26bc6ac02ab60f714d11f90e9dc94dc98eb58a11
Ben <Koppe.Development@gmail.com>
2024-11-03 13:12:37 -0500

    Disable newlines with comment prefix

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Remove formatoptions for newlines after comments
+-- Without this, the comment prefix is added after a new line in insert mode
+vim.api.nvim_create_autocmd("FileType", {
+  command = "set formatoptions-=ro",
+})
